45300c6d1fd707308c8216db61bf2192b10c53b6
Billy <95180027+billy-java@users.noreply.github.com>
2025-01-01 11:33:05 +0100

    mise a jour - (34)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,32 +7,33 @@ import { RootState_DB } from '../redux/store';
 const Navbar: React.FC = () => {
   const location = useLocation();
   const isEventPage = location.pathname.includes('/e/');
-
   const displayNavbarPages2 = ['/parametres', '/contact', '/aide', '/home'];
   const isMenu2Page = displayNavbarPages2.includes(location.pathname);
   const idEvenement = useSelector((state: RootState_DB) => state.auth.idEv);
   const hideNavbarRoutes = ['/', '/inscription', '/restaurer'];
 
-
   if (isMenu2Page) {
     return (
-      <nav className="fixed bottom-0 left-0 w-full bg-gray-800 text-white z-50">
-        <ul className="flex justify-around items-center py-3">
-          {menuParametre.map((item, index) => (
-            <li key={index}>
-              <Link
-                to={item.lien}
-                className={`flex items-center space-x-1 ${location.pathname.includes(item.lien) ? 'text-blue-400' : ''}`}>
-                {item.icon1} <span className="text-sm">{item.nom}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <nav className="fixed bottom-0 left-0 right-0 max-w-[430px] mx-auto bg-gray-800 text-white z-50">
+  <ul className="flex justify-around items-center py-3">
+    {menuParametre.map((item, index) => (
+      <li key={index}>
+        <Link
+          to={item.lien}
+          className={`flex items-center space-x-1 ${
+            location.pathname.includes(item.lien) ? 'text-blue-400' : ''
+          }`}>
+          {item.icon1}
+          <span className="text-sm">{item.nom}</span>
+        </Link>
+      </li>
+    ))}
+  </ul>
+</nav>
+
     );
   }
 
-  // Masquer la navbar sur la page Invitation
   if (location.pathname.includes('/invitation')) {
     return null;
   }
@@ -42,34 +43,35 @@ const Navbar: React.FC = () => {
   }
 
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-gray-800 text-white">
-      <ul className="flex justify-around items-center py-3">
-        {menu.map((item, index) =>
-          item.nom === 'Home' ? (
-            <li key={index}>
-              <Link
-                to={`${item.lien}`}
-                className={`flex items-center space-x-1 ${
-                  location.pathname.includes(item.lien) ? 'text-blue-400' : ''
-                }`}>
-                {item.icon1}
-              </Link>
-            </li>
-          ) : (
-            <li key={index}>
-              <Link
-                to={`/e/${idEvenement}${item.lien}`}
-                className={`flex items-center space-x-1 ${
-                  location.pathname.includes(item.lien) ? 'text-blue-400' : ''
-                }`}>
-                {item.icon1}{' '}
-                {location.pathname.includes(item.lien) && <p>{item.nom}</p>}
-              </Link>
-            </li>
-          )
-        )}
-      </ul>
-    </nav>
+    <nav className="fixed bottom-0 left-0 right-0 max-w-[430px] mx-auto bg-gray-800 text-white z-50">
+  <ul className="flex justify-around items-center py-3">
+    {menu.map((item, index) =>
+      item.nom === 'Home' ? (
+        <li key={index}>
+          <Link
+            to={`${item.lien}`}
+            className={`flex items-center space-x-1 ${
+              location.pathname.includes(item.lien) ? 'text-blue-400' : ''
+            }`}>
+            {item.icon1}
+          </Link>
+        </li>
+      ) : (
+        <li key={index}>
+          <Link
+            to={`/e/${idEvenement}${item.lien}`}
+            className={`flex items-center space-x-1 ${
+              location.pathname.includes(item.lien) ? 'text-blue-400' : ''
+            }`}>
+            {item.icon1}
+            {location.pathname.includes(item.lien) && <p>{item.nom}</p>}
+          </Link>
+        </li>
+      )
+    )}
+  </ul>
+</nav>
+
   );
 };
 
